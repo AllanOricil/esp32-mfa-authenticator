0df317db0e3a30c3a60b75808dc33de7c05d806c
Allan Oricil <allanoricilcos@outlook.com>
2024-04-28 02:16:35 -0300

    chore: wifi_ssid and wifi_password are no longer set with CHOCOLATE as their default value
    It is now required to set WIFI_SSID and WIFI_PASSWORD env variables before building the code.
    If env variables are not set, a compilation error is thrown during builds.

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,12 +1,17 @@
 #include <WiFi.h>
 
 #ifndef WIFI_SSID
-#define WIFI_SSID (char*)"CHOCOLATE"
+  #error "WIFI_SSID is not defined! Please define WIFI_SSID."
+#else
+  #pragma message "WIFI_SSID is set to: " WIFI_SSID
 #endif
 
 #ifndef WIFI_PASSWORD
-#define WIFI_PASSWORD (char*)"CHOCOLATE"
+  #error "WIFI_PASSWORD is not defined! Please define WIFI_PASSWORD."
+#else
+  #pragma message "WIFI_PASSWORD is set to: " WIFI_PASSWORD
 #endif
+
 
 void init_wifi(){
   if(WIFI_SSID == NULL || WIFI_PASSWORD == NULL){
