33ec2f2780fc66684dba4e4725573c95c7408d83
Allan Oricil <allanoricilcos@outlook.com>
2024-05-27 12:44:35 -0300

    refactor: improve variables names

--- src/touch.cpp
+++ src/touch.cpp
@@ -4,56 +4,52 @@
 #include "constants.h"
 
 XPT2046_Bitbang touchscreen(TOUCH_MOSI, TOUCH_MISO, TOUCH_CLK, TOUCH_CS);
-static lv_indev_drv_t indev_drv;
-static uint32_t lastTouchTime = 0;
-static bool isScreenOn = true;
-static bool wasReleased = true;
+static lv_indev_drv_t touchDriver;
+static uint32_t firstTouchTime = 0;
+static bool isDisplayOn = true;
 
-void on_touch_change(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+void on_touch_change(lv_indev_drv_t *touchDriver, lv_indev_data_t *touchData)
 {
     Point touchPoint = touchscreen.getTouch();
-    int16_t tmp_x = touchPoint.x;
-    int16_t tmp_y = touchPoint.y;
+    int16_t tmpTouchPointX = touchPoint.x;
+    int16_t tmpTouchPointY = touchPoint.y;
 
     // NOTE: swapping due to the rotation
-    touchPoint.x = tmp_y;
-    touchPoint.y = tmp_x;
+    touchPoint.x = tmpTouchPointY;
+    touchPoint.y = tmpTouchPointX;
 
     if (touchPoint.x >= 0 && touchPoint.x < LCD_WIDTH && touchPoint.y >= 0 && touchPoint.y < LCD_HEIGHT)
     {
-        data->state = LV_INDEV_STATE_PR;
-        data->point.x = touchPoint.x;
-        data->point.y = touchPoint.y;
+        uint32_t secondTouchTime = millis();
 
-        uint32_t currentTouchTime = millis();
+        touchData->state = LV_INDEV_STATE_PRESSED;
+        touchData->point.x = touchPoint.x;
+        touchData->point.y = touchPoint.y;
 
-        lv_obj_t *active_screen = lv_scr_act();
-        const char *active_screen_name = (const char *)lv_obj_get_user_data(active_screen);
-
-        if (strcmp(active_screen_name, TOTP_SCREEN_NAME) == 0)
+        lv_obj_t *activeDisplay = lv_scr_act();
+        const char *activeDisplayName = (const char *)lv_obj_get_user_data(activeDisplay);
+        if (strcmp(activeDisplayName, TOTP_SCREEN_NAME) == 0)
         {
-            if (wasReleased && currentTouchTime - lastTouchTime < DOUBLE_TOUCH_INTERVAL)
+            if (touchData->state == LV_INDEV_STATE_RELEASED && secondTouchTime - firstTouchTime < DOUBLE_TOUCH_INTERVAL)
             {
-                if (isScreenOn)
+                if (isDisplayOn)
                 {
                     ledcWrite(PWM_CHANNEL_BCKL, 0);
-                    isScreenOn = false;
                 }
                 else
                 {
                     ledcWrite(PWM_CHANNEL_BCKL, 0.5 * PWM_MAX_BCKL);
-                    isScreenOn = true;
                 }
+
+                isDisplayOn = !isDisplayOn;
             }
         }
 
-        lastTouchTime = currentTouchTime;
-        wasReleased = false;
+        firstTouchTime = secondTouchTime;
     }
     else
     {
-        data->state = LV_INDEV_STATE_REL;
-        wasReleased = true;
+        touchData->state = LV_INDEV_STATE_RELEASED;
     }
 }
 
@@ -66,10 +62,10 @@ void init_touch()
     // TODO: create screen to ease manual calibration
     touchscreen.setCalibration(TOUCH_X_MIN, TOUCH_Y_MIN, TOUCH_X_MAX, TOUCH_Y_MAX);
 
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.disp = lv_disp_get_default();
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = on_touch_change;
-    lv_indev_drv_register(&indev_drv); // registering device driver
+    lv_indev_drv_init(&touchDriver);
+    touchDriver.disp = lv_disp_get_default();
+    touchDriver.type = LV_INDEV_TYPE_POINTER;
+    touchDriver.read_cb = on_touch_change;
+    lv_indev_drv_register(&touchDriver);
     Serial.println("Touch driver initialized and registered.");
 }
