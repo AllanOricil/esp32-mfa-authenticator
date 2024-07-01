2de3113d65a8becfb869eaf541d50e85baf966d3
Allan Oricil <allanoricilcos@outlook.com>
2024-06-30 23:49:35 -0300

    refactor(manager): change page name from totps to services

--- site/pages/esp32/services.vue
+++ site/pages/esp32/services.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5 mb-5">
     <div class="row justify-content-center">
       <div class="col-md-6">
-        <h1 class="mb-4">Manage Services</h1>
+        <h1 class="mb-4">Services</h1>
         <div class="row">
           <Totps></Totps>
         </div>
