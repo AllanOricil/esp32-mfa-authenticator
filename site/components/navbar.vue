57c66998500bf394927f70c0785ecec076ba7ef9
Allan Oricil <allanoricilcos@outlook.com>
2024-06-30 23:47:14 -0300

    feat(manager): setup navbar and home page

---
+++ site/components/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <nuxt-link class="navbar-brand" to="/esp32">Home</nuxt-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/esp32/settings">
+              Settings
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/esp32/services"
+              >Services</nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
