c228a0c9d74537da3cd659982ca7033617cba61b
Allan Oricil <allanoricilcos@outlook.com>
2024-07-06 21:20:57 -0300

    ci: get rid of pnpm

--- scripts/install-node-deps.py
+++ scripts/install-node-deps.py
@@ -12,7 +12,7 @@ print("Running install-node-deps.py")
 
 # Check if Node and pnpm are installed
 check_installation("node")
-check_installation("pnpm")
+check_installation("npm")
 
 if not os.path.isdir("node_modules"):
-    subprocess.run(["pnpm", "install"])
+    subprocess.run(["npm", "install"])
