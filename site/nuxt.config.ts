41fdd654eedb8502d33ad546383d37510412c8af
Allan Oricil <allanoricilcos@outlook.com>
2025-01-02 10:57:38 -0300

    chore: trying to make dependencies external

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
       primary: "#ff0000",
     },
   },
+  experimental: {
+    externalVue: true,
+  },
   buildId: process.env.NUXT_BUILD_ID,
   devtools: { enabled: true },
   ssr: false,
@@ -13,6 +16,31 @@ export default defineNuxtConfig({
     compilerOptions: {
       isCustomElement: (tag) =>
         ["esp-web-install-button"].some((prefix) => tag.startsWith(prefix)),
+    },
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [
+          "vue",
+          "vue-draggable-next",
+          "vue-router",
+          "yup",
+          "pinia",
+          "uuid",
+          "js-yaml",
+          "axios",
+          "axios-retry",
+          "@material/web",
+        ],
+        output: {
+          globals: {
+            uuid: "uuid",
+            vue: "vue",
+            tslib: "tslib",
+          },
+        },
+      },
     },
   },
   modules: ["@pinia/nuxt"],
@@ -27,6 +55,50 @@ export default defineNuxtConfig({
       script: [
         {
           src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/vue@3.5.13/dist/vue.global.prod.js",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/vue-router@4.5.0/dist/vue-router.global.min.js",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/yup@1.6.1/+esm",
+          type: "module",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/uuid@11.0.3/+esm",
+          type: "module",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/pinia@2.3.0/+esm",
+          type: "module",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/vue-draggable-next@2.2.1/dist/vue-draggable-next.global.min.js",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/js-yaml@4.1.0/dist/js-yaml.min.js",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/axios@1.7.9/dist/axios.min.js",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/axios-retry@4.5.0/+esm",
+          type: "module",
+          tagPosition: "bodyClose",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/@material/web@1.2.0/focus/md-focus-ring.min.js",
+          type: "module",
           tagPosition: "bodyClose",
         },
       ],
