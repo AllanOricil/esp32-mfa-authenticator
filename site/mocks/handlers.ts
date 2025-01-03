2acc7d0c1d0bdbbe5517c0722883625db2eaab69
Allan Oricil <allanoricilcos@outlook.com>
2025-01-03 19:52:37 -0300

    feat(manager): improvements to the mock api
    - add mock for creating services
    - add validation using yup to simulate the actual server validations

--- site/mocks/handlers.ts
+++ site/mocks/handlers.ts
@@ -1,10 +1,83 @@
 import { http, HttpResponse } from "msw";
+import {
+  object,
+  array,
+  string,
+  number,
+  boolean,
+  type InferType,
+  ValidationError,
+} from "yup";
 
 const MAX_SESSION_TIME = 5;
 
+const CONFIG_PUT_SCHEMA = object({
+  wifi: object({
+    ssid: string().required("SSID is required"),
+    password: string().required("WiFi password is required"),
+  }),
+  authentication: object({
+    pin: object({
+      pin: string().nullable().notRequired(),
+      hash: string().nullable().notRequired(),
+    }),
+    unlock_attempts: number()
+      .default(3)
+      .typeError("Unlock attempts must be a number")
+      .positive("Unlock attempts must be positive")
+      .integer("Unlock attempts must be an integer")
+      .strict(),
+  }),
+  display: object({
+    sleep_timeout: number().nullable().notRequired(),
+  }),
+  touch: object({
+    calibrate: boolean().default(false),
+  }),
+});
+
+const SERVICES_PUT_SCHEMA = array().of(
+  object({
+    name: string().required("Name is required."),
+    group: number()
+      .typeError("Group must be a number")
+      .integer("Group must be an integer.")
+      .required("Group is required.")
+      .strict(),
+  })
+);
+
+const SERVICES_POST_SCHEMA = array().of(
+  object({
+    name: string()
+      .required("Name is required.")
+      .max(60, "Name must be 60 characters or fewer."),
+    secret: string()
+      .required("Secret is required.")
+      .test(
+        "is-multiple-of-8",
+        "Secret length must be a multiple of 8.",
+        (value) => {
+          return value.length % 8 === 0;
+        }
+      )
+      .test(
+        "is-base32",
+        "Secret must be a valid Base32-encoded string.",
+        (value) => {
+          return /^[A-Z2-7]+=*$/.test(value);
+        }
+      ),
+    group: number()
+      .typeError("Group must be a number")
+      .integer("Group must be an integer.")
+      .required("Group is required.")
+      .strict(),
+  })
+);
+
 export const handlers = [
-  http.post("/api/v1/auth/login", async ({ request }) => {
-    const requestBody = await request.json();
+  http.post("/api/v1/auth/login", async () => {
     const date = new Date();
     date.setTime(date.getTime() + MAX_SESSION_TIME * 60 * 1000);
 
@@ -21,7 +94,7 @@ export const handlers = [
 
     return response;
   }),
-  http.post("/api/v1/auth/logout", async ({ request }) => {
+  http.post("/api/v1/auth/logout", async () => {
     const response = new HttpResponse(
       JSON.stringify({ message: "logout successful" }),
       {
@@ -64,12 +137,38 @@ export const handlers = [
     );
   }),
   http.put("/api/v1/config", async () => {
-    return HttpResponse.json(
-      {
-        message: "configuration updated",
-      },
-      { status: 200 }
-    );
+    const config = await request.json();
+
+    try {
+      await CONFIG_PUT_SCHEMA.validate(config, { abortEarly: false });
+
+      return HttpResponse.json(
+        {
+          message: "services updated",
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        console.error("Validation error:", error.errors);
+        throw { type: "ValidationError", errors: error.errors };
+
+        return HttpResponse.json(
+          {
+            errors: error.errors,
+            code: "INVALID_BODY",
+          },
+          { status: 400 }
+        );
+      }
+
+      return HttpResponse.json(
+        {
+          message: "something went wrong",
+        },
+        { status: 500 }
+      );
+    }
   }),
 
   http.get("/api/v1/services", async () => {
@@ -109,5 +208,40 @@ export const handlers = [
       },
       { status: 200 }
     );
+  }),
+
+  http.post("/api/v1/services", async ({ request }) => {
+    const { services } = await request.json();
+
+    try {
+      await SERVICES_POST_SCHEMA.validate(services, { abortEarly: false });
+
+      return HttpResponse.json(
+        {
+          message: "services updated",
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        console.error("Validation error:", error.errors);
+        throw { type: "ValidationError", errors: error.errors };
+
+        return HttpResponse.json(
+          {
+            errors: error.errors,
+            code: "INVALID_BODY",
+          },
+          { status: 400 }
+        );
+      }
+
+      return HttpResponse.json(
+        {
+          message: "something went wrong",
+        },
+        { status: 500 }
+      );
+    }
   }),
 ];
