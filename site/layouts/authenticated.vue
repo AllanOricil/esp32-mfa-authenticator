44cec26f263713327343cff1d8803cfaab85534c
Allan Oricil <allanoricilcos@outlook.com>
2025-01-03 19:00:48 -0300

    refactor(manager): make toast globally available

--- site/layouts/authenticated.vue
+++ site/layouts/authenticated.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
     <Navbar />
+    <Toast ref="toast" />
     <slot />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Toast from "@/components/toast.vue";
+import { ref, provide } from "vue";
+
+const toast = ref(null);
+
+const showToast = (message: string, type: string = "bg-success") => {
+  if (toast.value) {
+    toast.value.show(message, type);
+  }
+};
+
+provide("showToast", showToast);
+</script>
+
+<style></style>
